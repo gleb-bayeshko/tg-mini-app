59e1ff877362ca9cbfc4491d136bb37e83f20dfc
gleb.bayeshko <gleb.bayeshko@scorum.com>
2023-09-26 22:32:50 +0300

    init: create a basic server structure

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ import express from 'express'
 import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
 
+import logger from './api/middlewares/loggerMiddleware'
+
 const PORT = process.env.PORT ?? 8000
 const BOT_TOKEN = process.env.BOT_TOKEN
 
@@ -13,10 +15,17 @@ if (!BOT_TOKEN) {
 const bot = new Telegraf(BOT_TOKEN)
 const app = express()
 
-bot.on(message('text'), ctx => ctx.reply('Hello'))
+app.use(logger)
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
 
 app.listen(PORT, () => {
   console.log('Listening on port:\t', PORT)
 })
 
+bot.on(message('text'), ctx => ctx.reply('Hello from bot!'))
 bot.launch()
