cb39d32d12fb3b1c74e9b608cf392d3a194e4ce2
gleb.bayeshko <gleb.bayeshko@scorum.com>
2023-10-02 01:47:21 +0300

    feat: implement promo banner and product categories

---
+++ client/src/features/products/productCategories/ProductCategoriesScroll/const.js
@@ -0,0 +1,50 @@
+export const productCategories = [
+  {
+    id: 1,
+    name: 'Food',
+    category: 'food',
+    imgSrc: '/images/productCategories/category-food.png',
+  },
+  {
+    id: 2,
+    name: 'Bowls',
+    category: 'bowls',
+    imgSrc: '/images/productCategories/category-bowls.png',
+  },
+  {
+    id: 3,
+    name: 'Fillers',
+    category: 'fillers',
+    imgSrc: '/images/productCategories/category-litter-box-fillers.png',
+  },
+  {
+    id: 4,
+    name: 'Hygiene',
+    category: 'hygiene',
+    imgSrc: '/images/productCategories/category-hygiene-and-care.png',
+  },
+  {
+    id: 5,
+    name: 'Vitamins',
+    category: 'vitamins',
+    imgSrc: '/images/productCategories/category-vitamins.png',
+  },
+  {
+    id: 6,
+    name: 'Toys',
+    category: 'toys',
+    imgSrc: '/images/productCategories/category-toys.png',
+  },
+  {
+    id: 7,
+    name: 'Clothes',
+    category: 'clothes',
+    imgSrc: '/images/productCategories/category-clothes.png',
+  },
+  {
+    id: 8,
+    name: 'Carriers',
+    category: 'carriers',
+    imgSrc: '/images/productCategories/category-carriers.png',
+  },
+]
