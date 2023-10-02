852d8344b2e8f15626641b07a607e80f08ec91cb
gleb.bayeshko <gleb.bayeshko@scorum.com>
2023-10-02 17:14:49 +0300

    feat: implement layout of main page

--- client/src/features/products/productCategories/ProductCategoriesPopular/ProductCategoriesPopular.jsx
+++ client/src/features/products/productCategories/ProductCategoriesPopular/ProductCategoriesPopular.jsx
@@ -1,8 +1,46 @@
+import { ArrowRightIcon } from 'shared/icons'
+import { Wrapper } from 'shared/ui/Wrapper'
+import { ProductCategoriesPopularItem } from './ProductCategoriesPopularItem'
+import { productCategoriesPopular, productCategoriesStyle } from './const'
 import './styles.css'
 
-function ProductCategoriesPopular(props) {
+function ProductCategoriesPopular() {
+  const renderCategories = () => productCategoriesPopular.map(({
+    id,
+    category,
+    imgSrc,
+    title,
+    description
+  }, i) => (
+    <a
+      key={id}
+      //TODO: Add actual routes
+      href="/"
+      className="product-categories-popular__href"
+    >
+      <ProductCategoriesPopularItem
+        className="product-categories-popular__item"
+        height={productCategoriesStyle[i]?.height}
+        color={productCategoriesStyle[i]?.color}
+        imgSrc={imgSrc}
+        title={title}
+        description={description}
+      >
+        {category === 'all' && (
+          <ArrowRightIcon className="product-categories-popular__arrow" />
+        )}
+      </ProductCategoriesPopularItem>
+    </a>
+  ))
+
   return (
-    <div></div>
+    <div className="product-categories-popular">
+      <Wrapper>
+        <div className="product-categories-popular-content">
+          {renderCategories()}
+        </div>
+      </Wrapper>
+    </div>
   )
 }
 
