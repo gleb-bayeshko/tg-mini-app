6058a1b1e396165b2c6669218520cf0b94d7f686
Gleb Bayeshko <60566178+gleb-bayeshko@users.noreply.github.com>
2023-10-06 01:22:32 +0300

    feat: implement client drawer (#2)
    * feat: implement base drawer
    
    * feat: implement drawer
    
    ---------
    
    Co-authored-by: gleb.bayeshko <gleb.bayeshko@scorum.com>

--- client/src/features/cart/CartButton/CartButton.jsx
+++ client/src/features/cart/CartButton/CartButton.jsx
@@ -1,21 +1,25 @@
+import { useState } from 'react'
 import { CartIcon } from 'shared/icons'
 import { getClassName } from 'shared/utils'
 import 'features/cart/CartButton/styles.css'
 
 function CartButton () {
   //TODO: Implement increase of products count with store
-  const num = 0
+  const [count, setCount] = useState(0)
 
   return (
     <div
-      key={num}
-      className={getClassName('cart', { 'cart_active': num > 0 })}
+      key={count}
+      className={getClassName('cart', { 'cart_active': count > 0 })}
+      onClick={() => {
+        setCount(prev => ++prev)
+      }}
     >
       <CartIcon className="cart-icon" />
       <div className="cart__background" />
       <div className="cart__animation-shadow" />
-      {num > 0 && (
-        <div className="cart__products-count">{num}</div>
+      {count > 0 && (
+        <div className="cart__products-count">{count}</div>
       )}
     </div>
   )
--- client/src/shared/ui/BurgerButton/BurgerButton.jsx
+++ client/src/shared/ui/BurgerButton/BurgerButton.jsx
@@ -1,43 +1,54 @@
-import { useRef,useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Lottie as BurgerMenuLottie } from 'shared/ui/Lottie'
 import { getClassName } from 'shared/utils'
 import burgerMenuAnimation from 'shared/assets/lotties/burgerMenu/burgerMenu.json'
 import './styles.css'
 
-function BurgerButton() {
+function BurgerButton({ className, onClick }) {
+  const [isButtonOpened, setIsButtonOpened] = useState(false)
+  const { isDrawerOpened } = useSelector(state => state.drawer)
   const ref = useRef()
 
-  const [isBurgerOpen, setIsBurgerOpen] = useState(false)
-
   const handleBurgerClick = () => {
-    setIsBurgerOpen((currentStatus) => !currentStatus)
-
-    if (!isBurgerOpen) {
-      ref.current?.playFirstPart()
-    } else {
-      ref.current?.playSecondPart()
-    }
+    onClick?.()
   }
+
+  useEffect(() => {
+    if (isDrawerOpened && !isButtonOpened) {
+      ref.current?.playFirstPart()
+      setIsButtonOpened(true)
+    }
+
+    if (!isDrawerOpened && isButtonOpened) {
+      ref.current?.playSecondPart()
+      setIsButtonOpened(false)
+    }
+  }, [isDrawerOpened, isButtonOpened])
 
   return (
     <div
-      className={getClassName('burger-button' , { 'burger-button_opened': isBurgerOpen } )}
+      className={getClassName('burger-button' , {
+        'burger-button_opened': isButtonOpened,
+        [`${className}`]: !!className
+      } )}
       onClick={handleBurgerClick}
     >
       <BurgerMenuLottie
         ref={ref}
         animationData={burgerMenuAnimation}
         styles={{ width: 55 }}
-        animationSpeed={1.7}
+        animationSpeed={2}
       />
     </div>
   )
 }
 
 BurgerButton.propTypes = {
-  onOpen: PropTypes.func,
-  onClose: PropTypes.func,
+  className: PropTypes.string,
+  isOpen: PropTypes.bool,
+  onClick: PropTypes.func,
 }
 
 export default BurgerButton
